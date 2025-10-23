b9b6942a5326ed39504d20255dda3938d9c8e740
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-10-23 21:46:17 +0000

    Unfinished

--- Main.java
+++ Main.java
@@ -4,6 +4,30 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter a word: ");
+    String userInput = scan.next();
+
+    String currentLetter = "";
+    int mostLetterInt = 0;
+    for(int i = 0; i < userInput.length(); i++)
+    {
+      currentLetter = userInput.substring(i, i + 1);
+      for(int j = 0; j < userInput.length(); j++)
+      {
+
+        if(currentLetter.equals(userInput.substring(j, j + 1)))
+        {
+          mostLetterInt++;
+        }
+      }
+
+    }
+
+    System.out.printf("The letter %s appears the most times with %d occurences.%n", currentLetter, mostLetterInt);
+
   }
+
 }
+
+
